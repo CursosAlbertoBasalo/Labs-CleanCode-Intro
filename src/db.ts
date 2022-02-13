dada4462dde41fcb726d11059ab3abc689f7fc91
Alberto Basalo <albertobasalo@hotmail.com>
2022-02-13 11:48:21 +0100

    Initialize 3-DIRTY-DATA

---
+++ src/db.ts
@@ -0,0 +1,21 @@
+export class DB {
+  public static select<T>(query: string): T[] {
+    console.log(query);
+    return [];
+  }
+
+  public static selectOne<T>(query: string): T {
+    console.log(query);
+    return {} as T;
+  }
+
+  public static insert<T>(dao: T): string {
+    console.log(dao);
+    return Date.now().toString();
+  }
+
+  public static update<T>(dao: T): number {
+    console.log(dao);
+    return 1;
+  }
+}
